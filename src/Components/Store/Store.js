aaa43a946d0275da2fc3a4529f5c40a28c3e585c
SSarker77$ <sarkershiladitya@gmail.com>
2025-01-17 12:39:17 +0530

    changes

---
+++ src/Components/Store/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterslice from "../../slice/counterslice";
+const store=configureStore({
+    reducer:{
+        counter:counterslice
+    }
+})
+export default store
